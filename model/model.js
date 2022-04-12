8d5405e16dfaf44ec86ead2fd64c63d068f33b2f
Emma <emersonpaige5@gmail.com>
2022-04-12 01:38:58 -0400

    final changes

--- model/model.js
+++ model/model.js
@@ -1,4 +1,4 @@
-var homeContent = `    <header class="header"></header>
+var homeContent = `<header class="header"></header>
 <div class="content">
   <div class="left">
     <div>
@@ -17,26 +17,22 @@ var homeContent = `    <header class="header"></header>
     <img src="images/2.png" alt="" />
   </div>
 </div>`;
-var aboutContent = `<div class="content3">
+var aboutContent = ` <div class="content3">
 <div>
-  <img class="headshot" src="images/head.png" alt="" />
+  <img class="headshot" src="images/headshot1.png" alt="" />
 </div>
 <div class="vl"></div>
 <div class="about">
   <h1>HI, I AM EMMA!</h1>
   <p class="content3p">
-    FASHION DESIGN IS AN OUTLET I USE TO EXPRESS MYSELF. THIS IS A PLACE
-    WHERE I CAN ORGANIZE ALL MY OUTFITS AND CLOTHES I HAVE DESIGNED. A
-    PLACE WHERE "ORGANIZATION MEETS CREATIVITY" FOLLOW ALONG FOR TIPS
-    AND INSPIRATION TO LEVEL UP YOUR CLOSET.
+    Fashion design is an outlet I use to express myself. This is a place
+    where I can organize all of my outfit and clothes I have designed. A
+    space where "Organization meets creativity". Follow along to
+    recreate my looks and level up your closet.
   </p>
 </div>
 </div>`;
-var shopContent = ` <div class="header1">
-<p>OUTFIT IDEAS TO WEAR ON VALENTINE'S DAY</p>
-</div>
-
-<div class="content4">
+var shopContent = `<div class="content4">
 <div class="right">
   <img src="images/standingroses.jpg" alt="" />
   <img src="images/natRose.jpg" alt="" />
@@ -68,8 +64,58 @@ var shopContent = ` <div class="header1">
 <a href="https://pin.it/6JlJyEF"
   ><img src="images/rednikes.png" alt="" class="picture1"
 /></a>
+</div>
+
+<div class="body">
+<div class="redtext">
+  <img src="images/neutralWord.png" alt="" />
+</div>
+<div>
+  <img class="allred" src="images/neutralOutfit.jpg" alt="" />
+</div>
+</div>
+<div class="ptagdiv">
+<p class="ptag">SHOP A SIMILAR LOOK BELOW</p>
+</div>
+<div class="body2">
+<a href="https://pin.it/5vZWYNO"
+  ><img class="picture1" src="images/fuzzyBucket.jpg" alt=""
+/></a>
+<a href="https://www.vagabond.com/us/cosmo-2-0-5349-501-20/"
+  ><img class="picture1" src="images/vengabond.jpg" alt=""
+/></a>
+<a href="https://pin.it/79p3A37"
+  ><img class="picture1" src="images/whiteShirt.jpg" alt=""
+/></a>
+<a
+  href="https://www.ralphlauren.com/women-clothing-pants/pleated-wool-twill-high-rise-pant/0044659712.html?utm_source=PerfSocial&utm_medium=All_Pinterest_Dynamic_258894759_123593053&pp=0&epik=dj0yJnU9VVBWVm5CcHZFenJuMkhhUWl4SGJYS2pibkM0b0J1V3AmcD0xJm49SEtvakN0UkJFV1Jhb0FJdGQ2SWRzUSZ0PUFBQUFBR0pWRDFB&dclid=CKWIusfmjfcCFQeDswodmZwK4A"
+  ><img src="images/greyPants.jpg" alt="" class="picture1"
+/></a>
 </div>`;
-var contactContent = `<h1>CONTACT</h1>`;
+var contactContent = ` <div class="contact">
+<h3>LET'S CONNECT!</h3>
+<!-- contact boxes -->
+<div class="boxesalign">
+  <div class="label">
+    <p>name:</p>
+    <div class="box"></div>
+  </div>
+  <div class="label">
+    <p>email:</p>
+    <div class="box"></div>
+  </div>
+  <div class="label">
+    <p>number:</p>
+    <div class="box"></div>
+  </div>
+
+  <div>
+    <div class="submit">
+      <div class="h4">submit</div>
+    </div>
+  </div>
+</div>
+</div>`;
 
 
 export function modelPageName (pgName){
